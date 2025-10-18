91b480719f841a0119da2ee08c32b2add09bee5c
qqueeeeee <82098305+qqueeeeee@users.noreply.github.com>
2025-10-18 15:18:55 +0530

    fix route 3

--- recommend_api.py
+++ recommend_api.py
@@ -5,6 +5,7 @@ from typing import List
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import requests
 
 app = FastAPI()
 
